23c1222015075dd9990f119ce63110e87f6d30be
Emanuel Palm <emanuelpalm@gmail.com>
2015-01-12 22:38:27 +0100

    Continued on nginx puppet manifest and configuration.

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,10 +1,7 @@
 class nginx {
-  group   { 'www-data': ensure => 'present' }
-  user    { 'www-data': ensure => 'present' }
   package { 'nginx':    ensure => 'installed', require => Exec['apt-update'] }
   service { 'nginx':    ensure => 'running' }
 
-  File { owner => 0, group => 0, mode => 0644 }
   file { '/etc/nginx/sites-available/default':
     source => 'puppet:///modules/nginx/nginx.conf',
   }
