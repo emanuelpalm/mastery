88fdd05dd3c7d97a220785db241df2530733bc24
Emanuel Palm <emanuelpalm@gmail.com>
2015-01-12 22:39:42 +0100

    Continued on mastery_client puppet manifest.

--- puppet/modules/mastery_client/manifests/init.pp
+++ puppet/modules/mastery_client/manifests/init.pp
@@ -1,3 +1,13 @@
-class mastery_client {
+class mastery_client($baseDir) {
+  file  { $baseDir:
+    ensure       => 'directory',
 
+    source       => '/opt/repository/client/build/release/',
+    sourceselect => 'all',
+    recurse      => true,
+
+    owner        => 'www-data',
+    group        => 'www-data',
+    mode         => 0644,
+  }
 }
