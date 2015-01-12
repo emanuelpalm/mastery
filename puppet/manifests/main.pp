dfd19077f4cd56321fe847ee78de06954781f174
Emanuel Palm <emanuelpalm@gmail.com>
2015-01-12 22:40:21 +0100

    Continued on main puppet manifest.

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -2,6 +2,34 @@ exec { 'apt-update':
   command => '/usr/bin/apt-get update',
 }
 
+File {
+  owner => 'root',
+  group => 'root',
+  mode => 0644
+}
+
+group {
+  'www-data': ensure => 'present'
+}
+user {
+  'www-data': ensure => 'present'
+}
+
 include motd
 include nginx
+include nodejs
+class { 'mysql':
+  root_password => sha1(fqdn_rand(4294967295)),
+}
+
+class { 'mastery_client':
+  baseDir => '/usr/share/nginx/html',
+}
+class { 'mastery_account':
+  baseDir => '/usr/local/share/mastery/account/',
+}
+
+file { '/usr/local/share/mastery/':
+  ensure => 'directory',
+}
 
