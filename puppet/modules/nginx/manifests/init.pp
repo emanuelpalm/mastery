5d35f7d32f684d832c9bcb80d2af110463a060d7
Emanuel Palm <emanuelpalm@gmail.com>
2015-01-15 21:55:11 +0100

    Improved nginx puppet manifest.

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -2,19 +2,18 @@ class nginx {
   package { 'nginx':
     ensure => 'installed',
     require => Exec['apt-update'],
-  }
-  service { 'nginx':
-    ensure  => 'running',
-    require => File['/etc/nginx/sites-available/default'],
-  }
-
+  } ->
   file { '/etc/nginx/sites-available/default':
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
-  }
+  } ->
   file { '/etc/nginx/sites-enabled/default':
     ensure  => 'link',
     target  => '/etc/nginx/sites-available/default',
     require => File['/etc/nginx/sites-available/default'],
+  } ->
+  service { 'nginx':
+    ensure  => 'running',
+    require => File['/etc/nginx/sites-enabled/default'],
   }
 }
